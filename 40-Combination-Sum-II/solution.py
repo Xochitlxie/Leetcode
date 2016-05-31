b1292ca8e8d8c7d6d4560758722553767a26b886
Haohan Xie <haohanx@umich.edu>
2016-05-31 03:53:55 -0400

    [40. Combination Sum II][Runtime Error]committed by LeetCode Extension

---
+++ 40-Combination-Sum-II/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    self.result = []
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidated.sort()
+        self
+        
+    def dfs(self, candidates, index, path, target):
+        if target == 0:
+            result.append(path)
+            return
+        else:
+            for i in range(index+1,len(candidates)):
+                if candidates[i] > target:
+                    break
+                else:
+                    self.dfs（candidates,index,path.append(candidates[i]),target-candidates[i])
+        
