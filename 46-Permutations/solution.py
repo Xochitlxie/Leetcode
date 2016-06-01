a8b4841ce6ed130b3aa9756c1884399fad81b8ba
Haohan Xie <haohanx@umich.edu>
2016-06-01 16:20:44 -0400

    [46. Permutations][Accepted]committed by LeetCode Extension

---
+++ 46-Permutations/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.dfs(nums,[],result)
+        return result
+    
+    def dfs(self,nums,path,result):
+        if not nums:
+            result.append(path)
+        for i in xrange(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:],path+[nums[i]],result)
