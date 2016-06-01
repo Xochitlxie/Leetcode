c5e7646e6b2d3a59fbf2112916f303f6fd9573bd
Haohan Xie <haohanx@umich.edu>
2016-06-01 02:56:49 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.py
+++ 139-Word-Break/solution.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         d = [False] * len(s)    
         for i in range(len(s)):
-            for w in words:
+            for w in wordDict:
                 if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
                     d[i] = True
         return d[-1]
