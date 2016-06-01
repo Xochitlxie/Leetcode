432a2e949f87ca48965efb3b904e1eb36df32a2b
Haohan Xie <haohanx@umich.edu>
2016-06-01 16:27:17 -0400

    [47. Permutations II][Runtime Error]committed by LeetCode Extension

---
+++ 47-Permutations-II/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.dfs(nums,path,result)
+        return set(result)
+        
+        
+    def dfs(self,nums,path,result):
+        if not nums:
+            result.append(path)
+        for i in xrange(nums):
+            self.dfs(nums[:i]+nums[i+1:],path+[nums[i]],result)
