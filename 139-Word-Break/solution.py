f72c65998a33eccca8d6fe977ee1396b658e75f9
Haohan Xie <haohanx@umich.edu>
2016-06-01 02:56:35 -0400

    [139. Word Break][Runtime Error]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: bool
+        """
+        d = [False] * len(s)    
+        for i in range(len(s)):
+            for w in words:
+                if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+                    d[i] = True
+        return d[-1]
