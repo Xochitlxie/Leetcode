bbf524d434d029a2f477f733f341f11c77ee0c95
Haohan Xie <haohanx@umich.edu>
2016-06-02 04:29:02 -0400

    [213. House Robber II][Accepted]committed by LeetCode Extension

--- 213-House-Robber-II/solution.py
+++ 213-House-Robber-II/solution.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 1:
+            return nums[0]
         return max(self.dp(nums[:-1]),self.dp(nums[1:]))
         
     def dp(self,nums):
+        if not nums:
+            return 0
         rob = nums[0]
         notRob = 0
         for i in range(1,len(nums)):
