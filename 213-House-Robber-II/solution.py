8b7abdf2a394cb4d580628535a0901c36c255fd2
Haohan Xie <haohanx@umich.edu>
2016-06-02 04:27:32 -0400

    [213. House Robber II][Runtime Error]committed by LeetCode Extension

---
+++ 213-House-Robber-II/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return max(self.dp(num[:-1]),self.dp(num[1:]))
+        
+    def dp(self,nums):
+        rob = nums[0]
+        notRob = 0
+        for i in range(1:len(nums)):
+            rob,notRob = notRob + nums[i], max(rob,notRob)
+        return max(rob,notRob)
+            
