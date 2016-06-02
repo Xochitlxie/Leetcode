d28d784498bd27a428bff24204025ee2203f5953
Haohan Xie <haohanx@umich.edu>
2016-06-02 18:22:27 -0400

    [78. Subsets][Runtime Error]committed by LeetCode Extension

--- 78-Subsets/solution.py
+++ 78-Subsets/solution.py
@@ -5,16 +5,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         result = [[]]
-        if not nums:
-            return result
-        nums.sort()
-        for i in range(1,len(nums)+1):
-            self.dfs(i,nums,[],result)
+        for num in nums:
+            result += [subset+num for subset in results]
         return result
-        
-    def dfs(self,capacity,nums,subset,result):
-        if capacity == 0:
-            result.append(subset)
-            return
-        for i in range(len(nums)):
-            self.dfs(capacity-1,nums[i+1:],subset+[nums[i]],result)
