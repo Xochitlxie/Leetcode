47541940321209e2c478521c58de63803ebedd0d
Haohan Xie <haohanx@umich.edu>
2016-06-05 00:21:05 -0700

    [254. Factor Combinations][Runtime Error]committed by LeetCode Extension

--- 254-Factor-Combinations/solution.py
+++ 254-Factor-Combinations/solution.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-        def factor(n, i, combi, combis):
+        def getFactors(self, n):
+        todo, combis = [(n, 2, [])], []
+        while todo:
+            n, i, combi = todo.pop()
             while i * i <= n:
                 if n % i == 0:
                     combis += combi + [i, n/i],
-                    factor(n/i, i, combi+[i], combis)
+                    todo += (n/i, i, combi+[i]),
                 i += 1
-            return combis
-        return factor(n, 2, [], [])
+        return combis
