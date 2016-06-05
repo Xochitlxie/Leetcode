2e150b586337938cd462f1e5e815d4f61ca1fd0e
Haohan Xie <haohanx@umich.edu>
2016-06-04 19:54:01 -0700

    [254. Factor Combinations][Accepted]committed by LeetCode Extension

---
+++ 254-Factor-Combinations/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getFactors(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        def factor(n, i, combi, combis):
+            while i * i <= n:
+                if n % i == 0:
+                    combis += combi + [i, n/i],
+                    factor(n/i, i, combi+[i], combis)
+                i += 1
+            return combis
+        return factor(n, 2, [], [])
