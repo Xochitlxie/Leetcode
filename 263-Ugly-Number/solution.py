9a2fb5170b07f3ceb8babd1e68a89c30a180ed9e
Haohan Xie <haohanx@umich.edu>
2016-06-05 00:38:14 -0700

    [263. Ugly Number][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 263-Ugly-Number/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        uglyFactor = (2,3,5)
+        for i in uglyFactor:
+            while num%i == 0:
+                num = num/i
+        return num == 1    
+        
