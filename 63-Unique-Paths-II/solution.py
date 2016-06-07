068735de4ebcb9ec4f13b8108dcac958bbddfaae
Haohan Xie <haohanx@umich.edu>
2016-06-06 17:34:42 -0700

    [63. Unique Paths II][Runtime Error]committed by LeetCode Extension

--- 63-Unique-Paths-II/solution.py
+++ 63-Unique-Paths-II/solution.py
@@ -9,7 +9,7 @@ class Solution(object):
             for j in range(len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
                     dp[i][j] = 0
-        for i in range(1,len(dp))):
+        for i in range(1,len(dp)):
             for j in range(1,len(dp[0])):
                 dp = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]
