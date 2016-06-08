c08eb78f1a3a387449da80f0b77e8b9cc50ae2cb
Haohan Xie <haohanx@umich.edu>
2016-06-07 23:02:54 -0700

    [304. Range Sum Query 2D - Immutable][Runtime Error]committed by LeetCode Extension

---
+++ 304-Range-Sum-Query-2D---Immutable/solution.py
@@ -0,0 +1,45 @@
+class NumMatrix(object):
+    def __init__(self, matrix):
+        """
+        initialize your data structure here.
+        :type matrix: List[List[int]]
+        """
+        if not matrix or not matrix[0]:
+            return []
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0 for i in range(n)] for j in range(m)]
+        dp[0][0] = matrix[0][0]
+        for i in range(1,n):
+            dp[0][i] = dp[0][i-1] + matrix[0][i]
+        for i in range(1,m):
+            dp[i][0] = dp[i-1][0] + matrix[i][0]
+        for i in range(1,m):
+            for i in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        self.sum = dp
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        if row1 == 0 and col1 == 0:
+            return self.sum[row2][col2]
+        elif row1 == 0:
+            return self.sum[row2][col2] - self.sum[row2][col1-1]
+        elif col1 == 0:
+            return self.sum[row2][col2] - self.sum[row1-1][cow2]
+        else:
+            return self.sum[row2][col2] - self.sum[row1-1][cow2] - self.sum[row2][col1-1] + self.sum[r1-1][c1-1]
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# numMatrix = NumMatrix(matrix)
+# numMatrix.sumRegion(0, 1, 2, 3)
+# numMatrix.sumRegion(1, 2, 3, 4)
