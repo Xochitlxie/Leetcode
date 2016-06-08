1a131c54ed5163ca133016f380b5848fd636ad48
Haohan Xie <haohanx@umich.edu>
2016-06-08 00:13:47 -0700

    [120. Triangle][Accepted]committed by LeetCode Extension

--- 120-Triangle/solution.py
+++ 120-Triangle/solution.py
@@ -7,6 +7,6 @@ class Solution(object):
         res = triangle[-1]
         for i in range(len(triangle)-2,-1,-1):
             for j in range(len(triangle[i])):
-                res[j] = triangle[i][j] + min(res[j+1],res[j]）
+                res[j] = triangle[i][j] + min(res[j+1],res[j])
         return res[0]
             
