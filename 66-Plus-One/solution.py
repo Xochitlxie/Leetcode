7b75221ba6dfd717546be68bd3a19819af33d4ae
Haohan Xie <haohanx@umich.edu>
2016-06-09 13:42:39 -0700

    [66. Plus One][Accepted]committed by LeetCode Extension

--- 66-Plus-One/solution.py
+++ 66-Plus-One/solution.py
@@ -10,5 +10,18 @@ class Solution(object):
             num = num*10 + digits[i]
         return [int(i) for i in str(num+1)]
                 
-        
+"""
+def plusOne(digits):
+    digits[-1] += 1
+    for i in range(len(digits)-1, 0, -1):
+        if digits[i] != 10:
+            break
+        digits[i] = 0
+        digits[i-1] += 1
+
+    if digits[0] == 10:
+        digits[0] = 0
+        return [1] + digits
+    return digits
+"""
         
