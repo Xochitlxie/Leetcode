f28cf1377e31e751114c82b622a8741dbc2074b3
Haohan Xie <haohanx@umich.edu>
2016-06-09 01:05:57 -0700

    [128. Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 128-Longest-Consecutive-Sequence/solution.py
+++ 128-Longest-Consecutive-Sequence/solution.py
@@ -12,9 +12,6 @@ class Solution(object):
                 right = dict.get(num+1,0)
                 dict[num-left] = 1 + left + right
                 dict[num+right] = 1 + left + right
-        #for i in dict.keys():
-        #    print i
-        #    print dict[i]
         maxLen = 0
         for key in dict.keys():
             maxLen = max(maxLen,dict[key])
