fe9bc3a8e7c322a10e13dfb48235d3fe93cd2b4f
Haohan Xie <haohanx@umich.edu>
2016-06-09 01:26:17 -0700

    [298. Binary Tree Longest Consecutive Sequence][Runtime Error]committed by LeetCode Extension

--- 298-Binary-Tree-Longest-Consecutive-Sequence/solution.py
+++ 298-Binary-Tree-Longest-Consecutive-Sequence/solution.py
@@ -11,29 +11,25 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        count = []
+        maxLen = [0]
         if root:
-            self.dfs(root,1,count)
-        maxLen = 0
-        print count
-        for i in count:
-            maxLen = max(maxLen,i)
-        return maxLen
+            self.dfs(root,1,maxLen)
+        return maxLen[0]
         
-    def dfs(self,root,length,count):
+    def dfs(self,root,length,maxLen):
         if not root.left and not root.right:
-            count.append(length)
+            maxLen[0] = max(maxLen[0],length)
             return
         if root.left:
             if root.left.val == root.val + 1:
-                self.dfs(root.left,length+1,count)
+                self.dfs(root.left,length+1,maxLen[0])
             else:
-                count.append(length)
-                self.dfs(root.left,1,count)
+                maxLen[0] = max(maxLen[0],length)
+                self.dfs(root.left,1,maxLen)
         if root.right:
             if root.right.val == root.val + 1:
-                self.dfs(root.right,length+1,count)
+                self.dfs(root.right,length+1,maxLen)
             else:
-                count.append(length)
-                self.dfs(root.right,1,count)
+                maxLen[0] = max(maxLen[0],length)
+                self.dfs(root.right,1,maxLen)
         
