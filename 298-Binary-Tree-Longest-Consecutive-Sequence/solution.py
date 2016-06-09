0dd9afae86ad6c7af9e2ec8f8522190c76f82dbb
Haohan Xie <haohanx@umich.edu>
2016-06-09 01:31:20 -0700

    [298. Binary Tree Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 298-Binary-Tree-Longest-Consecutive-Sequence/solution.py
+++ 298-Binary-Tree-Longest-Consecutive-Sequence/solution.py
@@ -13,13 +13,10 @@ class Solution(object):
         """
         maxLen = [0]
         if root:
-            self.dfs(root,0 , root.val + 2, maxLen)
+            self.dfs(root,1,maxLen)
         return maxLen[0]
         
-    def dfs(self,root,length, parent_val ,maxLen):
-        if not root:
-            return
-        """
+    def dfs(self,root,length,maxLen):
         if not root.left and not root.right:
             maxLen[0] = max(maxLen[0],length)
             return
@@ -35,12 +32,4 @@ class Solution(object):
             else:
                 maxLen[0] = max(maxLen[0],length)
                 self.dfs(root.right,1,maxLen)
-        """
-        if root.val == parent_val + 1:
-            length += 1
-        else:
-            length = 1
-        maxLen[0] = max(maxLen[0], length)
-        self.dfs(root.right, length, root.val, maxLen)
-        self.dfs(root.left, length, root.val, maxLen)
         
