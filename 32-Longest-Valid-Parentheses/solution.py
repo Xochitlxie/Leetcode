fecf5107bdc5523dbc777023d6c42fbb4f9c21db
Haohan Xie <haohanx@umich.edu>
2016-06-13 02:46:29 -0700

    [32. Longest Valid Parentheses][Runtime Error]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.py
+++ 32-Longest-Valid-Parentheses/solution.py
@@ -4,7 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dic = {}
-        for s in strs:
-            dic[tuple(sorted(s))] = dic.get(tuple(sorted(s)),[]) + [s]
-        return [sorted(item) for item in dic.values()]
+        stack, res, s = [0], 0, ')'+s
+        for i in xrange(1, len(s)):
+            if s[i] == ')' and s[stack[-1]] == '(':
+                stack.pop()
+                res = max(res, i - stack[-1])
+            else:
+                stack.append(i)
+        return res
