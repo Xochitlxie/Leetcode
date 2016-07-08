ff3777469757cd2f047a5ad1b07313fcabbf895f
Haohan Xie <haohanx@umich.edu>
2016-07-08 14:30:49 -0700

    [348. Design Tic-Tac-Toe][Runtime Error]committed by LeetCode Extension

--- 348-Design-Tic-Tac-Toe/solution.py
+++ 348-Design-Tic-Tac-Toe/solution.py
@@ -9,20 +9,20 @@ class TicTacToe(object):
         
             
         def move(self, row, col, player):
-        """
-        Player {player} makes a move at ({row}, {col}).
-        @param row The row of the board.
-        @param col The column of the board.
-        @param player The player, can be either 1 or 2.
-        @return The current winning condition, can be either:
+            """
+            Player {player} makes a move at ({row}, {col}).
+            @param row The row of the board.
+            @param col The column of the board.
+            @param player The player, can be either 1 or 2.
+            @return The current winning condition, can be either:
                 0: No one wins.
                 1: Player 1 wins.
                 2: Player 2 wins.
-        :type row: int
-        :type col: int
-        :type player: int
-        :rtype: int
-        """
+            :type row: int
+            :type col: int
+            :type player: int
+            :rtype: int
+            """
             for i, x in enumerate((row, col, row+col, row-col)):
                 count[i, x, player] += 1
                 if count[i, x, player] == n:
