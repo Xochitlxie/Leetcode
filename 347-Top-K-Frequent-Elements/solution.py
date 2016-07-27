343b5c90e3b5a3c5fb496a6e4184782eef6ba11e
Haohan Xie <haohanx@umich.edu>
2016-07-26 22:02:02 -0700

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.py
+++ 347-Top-K-Frequent-Elements/solution.py
@@ -6,9 +6,24 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        most_item = Counter(nums).most_common(k)
+        frequency = {}
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
+        
+        bucket = [None] * (len(nums) + 1)
+        for (num, freq) in frequency.items():
+            if bucket[freq] == None:
+                bucket[freq] = []
+            bucket[freq].append(num)
+        
         result = []
-        for i in most_item:
-            result.append(i[0])
+        for i in reversed(range(len(nums) + 1)):
+            if k > 0:
+                if bucket[i] is not None:
+                    result.extend(bucket[i])
+                    k -= len(bucket[i])
+            else:
+                break
+            
         return result
     
