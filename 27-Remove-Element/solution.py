25b40e0febbb4a540cdac8eceb38b80cf3e0923f
Haohan Xie <haohanx@umich.edu>
2016-07-31 00:03:16 -0700

    [27. Remove Element][Runtime Error]committed by LeetCode Extension

--- 27-Remove-Element/solution.py
+++ 27-Remove-Element/solution.py
@@ -7,11 +7,11 @@ class Solution(object):
         """
         start, end = 0, len(nums)-1
         while start < end:
-            while nums[end] == val:
+            while nums[end] == val and end >= 0:
                 end -= 1
             if nums[start] != val:
                 start += 1
             else:
                 nums[start],nums[end] = nums[end],nums[start]
                 end -= 1
-        return start
+        return nums[:start+1]
