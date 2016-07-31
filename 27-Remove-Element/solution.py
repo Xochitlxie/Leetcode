665dfd271b7f8b86a90dbb5afe17326a1e80a62d
Haohan Xie <haohanx@umich.edu>
2016-07-31 00:05:19 -0700

    [27. Remove Element][Accepted]committed by LeetCode Extension

--- 27-Remove-Element/solution.py
+++ 27-Remove-Element/solution.py
@@ -5,13 +5,10 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        start, end = 0, len(nums)-1
-        while start < end:
-            while nums[end] == val and end >= 0:
-                end -= 1
-            if nums[start] != val:
-                start += 1
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end], end = nums[end], nums[start], end - 1
             else:
-                nums[start],nums[end] = nums[end],nums[start]
-                end -= 1
-        return nums[:start+1]
+                start +=1
+        return start
