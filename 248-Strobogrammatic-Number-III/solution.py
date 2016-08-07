ce23dc9408d5b5ee3e68d2ffc8bcaaa262e7c6c3
Haohan Xie <haohanx@umich.edu>
2016-08-06 19:47:32 -0700

    [248. Strobogrammatic Number III][Runtime Error]committed by LeetCode Extension

--- 248-Strobogrammatic-Number-III/solution.py
+++ 248-Strobogrammatic-Number-III/solution.py
@@ -6,22 +6,24 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        def dfs(low,high,c,left,right):
-            if left > right:
-                s = "".join(c)
-                if (len(s) == len(low) and int(s) < int(low)) or (len(s) == len(high) and int(s) > int(high)):
-                    return
-                count += 1
-                return
-            for p in pairs:
-                c[left] = p[0]
-                c[right] = p[1]
-                if len(c) != 1 and c[0] == 0:
-                    continue
-                if left < right or (left == right and p[0] == p[1]):
-                    dfs(low,high,c,left+1,right-1)
-
         pairs = ["00","11","69","88","96"]
         for i in range(len(low),len(high)+1):
-            dfs(low,high,["0"]*i,0,i-1)
+            self.dfs(low,high,["0"]*i,0,i-1)
         return count
+        
+    def dfs(self.low,high,c,left,right):
+        if left > right:
+            s = "".join(c)
+            if (len(s) == len(low) and int(s) < int(low)) or (len(s) == len(high) and int(s) > int(high)):
+                return
+            count += 1
+            return
+        for p in pairs:
+            c[left] = p[0]
+            c[right] = p[1]
+            if len(c) != 1 and c[0] == 0:
+                continue
+            if left < right or (left == right and p[0] == p[1]):
+                self.dfs(low,high,c,left+1,right-1)
+
+        
