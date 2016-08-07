5333a6318e6a55f33c3704adea4ae10ad24d9e3f
Haohan Xie <haohanx@umich.edu>
2016-08-06 19:57:34 -0700

    [323. Number of Connected Components in an Undirected Graph][Accepted]committed by LeetCode Extension

--- 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.py
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-    p = range(n)
-    def find(x):
-        if p[x] != x:
-            p[x] = find(p[x])
-        return p[x]
+        p = range(n)
+        def find(x):
+            if p[x] != x:
+                p[x] = find(p[x])
+            return p[x]
     
-    for e in edges:
-        v, w = map(find, e)
-        p[v] = w
-        n -= v != w
-    return n
+        for e in edges:
+            v, w = map(find, e)
+            p[v] = w
+            n -= v != w
+        return n
