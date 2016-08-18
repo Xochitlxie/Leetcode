d13a11f1972045b77f1158431d1f1a20d8e8e61c
Haohan Xie <haohanx@umich.edu>
2016-08-17 17:58:42 -0700

    [136. Single Number][Accepted]committed by LeetCode Extension

--- 136-Single-Number/solution.py
+++ 136-Single-Number/solution.py
@@ -4,16 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ret = {}
-        result = 0;
+        ret = 0
         for i in nums:
-            if i in ret:
-                ret[i]=0;
-            else:
-                ret[i]=1;
-
-        for v in ret.keys():
-            if(ret[v]==1): 
-                result = v
-
-        return result
+            ret ^= i
+        return ret
