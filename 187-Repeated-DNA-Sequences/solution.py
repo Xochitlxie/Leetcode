c52786b8629b63aa960487a71da518fbb6301cd5
Haohan Xie <haohanx@umich.edu>
2016-08-22 01:24:58 -0700

    [187. Repeated DNA Sequences][Accepted]committed by LeetCode Extension

--- 187-Repeated-DNA-Sequences/solution.py
+++ 187-Repeated-DNA-Sequences/solution.py
@@ -10,7 +10,6 @@ class Solution(object):
         dict = {}
         for i in range(10,len(s)+1):
             dict[s[i-10:i]] = dict.get(s[i-10:i],0) + 1
-            print i,dict[s[i-10:i]],s[i-10:i]
         for key in dict.keys():
             if dict[key] >= 2:
                 result.append(key)
