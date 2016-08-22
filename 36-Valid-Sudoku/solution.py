a95262be5488fb177d1a457838702b9bc132cd51
Haohan Xie <haohanx@umich.edu>
2016-08-22 01:33:24 -0700

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.py
+++ 36-Valid-Sudoku/solution.py
@@ -8,7 +8,9 @@ class Solution(object):
         for i,row in enumerate(board):
             for j,c in enumerate(row):
                 if c!= ".":
-                    seen.append((c,j),(i,c),(i/3,j/3,c))
+                    seen.append((c,j))
+                    seen.append((i,c))
+                    seen.append((i/3,j/3,c))
         return len(seen) == len(set(seen))
         
         
