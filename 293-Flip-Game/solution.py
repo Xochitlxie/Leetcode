d7c1136cc18c0ea9edb1eae5628ef5b054fe0cdf
Haohan Xie <haohanx@umich.edu>
2016-08-22 01:58:50 -0700

    [293. Flip Game][Accepted]committed by LeetCode Extension

--- 293-Flip-Game/solution.py
+++ 293-Flip-Game/solution.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        answer = []
-        if len(s) < 2:
-            return answer
-        for i in range(0,len(s)-1):
-            newString = ''
-            if s[i]=='+' and s[i+1] == '+':
-                newString = s[0:i] + '--' + s[i+2:len(s)]
-                answer.append(newString)
-        return answer
+        result = []
+        if len(s) <= 1:
+            return result
+        for i in range(1,len(s)):
+            if s[i-1:i+1] == "++":
+                result.append(s[:i-1] + "--" + s[i+1:])
+        return result
