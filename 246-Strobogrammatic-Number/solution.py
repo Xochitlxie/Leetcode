7f4026beb67accac007bb9b57b20b8c0c6759682
Haohan Xie <haohanx@umich.edu>
2016-08-22 01:41:24 -0700

    [246. Strobogrammatic Number][Runtime Error]committed by LeetCode Extension

--- 246-Strobogrammatic-Number/solution.py
+++ 246-Strobogrammatic-Number/solution.py
@@ -4,21 +4,19 @@ class Solution(object):
         :type num: str
         :rtype: bool
         """
-        acceptable = ['1','6','9','8','0']
+        acceptNumber = ["1","6","9","8","0"]
         counter = {}
-        for c in acceptable:
-            if c is '6':
-                counter[c] = '9'
-            elif c is '9':
-                counter[c] = '6'
+        for i in acceptNumber:
+            if c=="6":
+                counter[c] == "9"
+            elif c== "9":
+                counter[c] == "6"
             else:
-                counter[c] = c
-
-        for i in range(len(num)):
-            if num[i] in counter:
-                if num[len(num)-i-1] != counter[num[i]]:
+                counter[c] == c
+        for i in range(len(nums)):
+            if nums[i] in acceptNumber:
+                if counter[i] != len(nums)-i-1:
                     return False
             else:
                 return False
         return True
-        
