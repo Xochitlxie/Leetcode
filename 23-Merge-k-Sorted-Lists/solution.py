8a69f2874485c95bb46b249d640c86b8b9365a5e
Haohan Xie <haohanx@umich.edu>
2016-08-28 16:06:13 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.py
+++ 23-Merge-k-Sorted-Lists/solution.py
@@ -13,14 +13,11 @@ class Solution(object):
         """
         dummy = ListNode(None)
         curr = dummy
-        q = PriorityQueue() 
+        q = PriorityQueue()
         for node in lists:
-            if node:
-                q.put((node.val,node))
-            while q.qsize()>0:
-                curr.next = q.get()[1]
-                curr = curr.next
-                if curr.next:
-                    q.put((cur.next.val,curr.next))
+            if node: q.put((node.val,node))
+        while q.qsize()>0:
+            curr.next = q.get()[1]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val, curr.next))
         return dummy.next
-        
