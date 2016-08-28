81748779015ac83cc6beaea8e6df3e84caab609f
Haohan Xie <haohanx@umich.edu>
2016-08-28 16:00:10 -0700

    [23. Merge k Sorted Lists][Runtime Error]committed by LeetCode Extension

---
+++ 23-Merge-k-Sorted-Lists/solution.py
@@ -0,0 +1,26 @@
+from Queue import PriorityQueue
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        dummy = ListNode(None)
+        curr = dummy
+        q = PriorityQueue() 
+        for node in lists:
+            if node:
+                q.put((node.val,node))
+            while q.qsize()>0:
+                curr.next = q.get[1]
+                curr = curr.next
+                if curr.next:
+                    q.put((cur.next.val,curr.next))
+        return dummy.next
+        
