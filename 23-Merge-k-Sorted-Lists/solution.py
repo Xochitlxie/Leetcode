733317ab4ff4e36e21f78b3d92fb2fc73dc524b5
Haohan Xie <haohanx@umich.edu>
2016-08-28 16:00:46 -0700

    [23. Merge k Sorted Lists][Runtime Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.py
+++ 23-Merge-k-Sorted-Lists/solution.py
@@ -18,7 +18,7 @@ class Solution(object):
             if node:
                 q.put((node.val,node))
             while q.qsize()>0:
-                curr.next = q.get[1]
+                curr.next = q.get()[1]
                 curr = curr.next
                 if curr.next:
                     q.put((cur.next.val,curr.next))
