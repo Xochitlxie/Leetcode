506413974f04bec69dd6e90854cb53823429f26e
Haohan Xie <haohanx@umich.edu>
2016-08-30 01:05:16 -0700

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.py
+++ 91-Decode-Ways/solution.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         if s == "": return 0
-        dp = [0] * len(s)
+        dp = [0] * (len(s)+1)
         dp[0] = 1
         for i in range(1, len(s)+1):
             if s[i-1] != "0":
