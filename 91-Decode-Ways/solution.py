c58644d01578d5098459a84cd8f5c54626010684
Haohan Xie <haohanx@umich.edu>
2016-08-30 01:01:04 -0700

    [91. Decode Ways][Runtime Error]committed by LeetCode Extension

--- 91-Decode-Ways/solution.py
+++ 91-Decode-Ways/solution.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s) == 0:
-            return 0
-        if s[0]  == '0':
-            dp = [0] * len(s)
-        else:
-            dp = [1] + [0] * (len(s)-1)
-        for i in range(1,len(dp)):
-            if int(s[i-1]+s[i]) >= 1 and int(s[i-1] + s[i]) <=26 and int(s[i-1]+s[i]) != int(s[i]):
-                dp[i] = dp[i-1] + 1
-            else:
-                dp[i] = dp[i-1]
-        retrun dp[-1]
+        if s == "": return 0
+        dp = [0] * len(s)
+        dp[0] = 1
+        for i in range(1, len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if i != 1 and s[i-2:i] < "27" and s[i-2:i] > "09":  #"01"ways = 0
+                dp[i] += dp[i-2]
+        return dp[len(s)]
