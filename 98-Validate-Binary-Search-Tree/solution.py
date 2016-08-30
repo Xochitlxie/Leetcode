2404c81784c9bd2624baa58cec39ef119d3ba554
Haohan Xie <haohanx@umich.edu>
2016-08-30 01:16:46 -0700

    [98. Validate Binary Search Tree][Runtime Error]committed by LeetCode Extension

---
+++ 98-Validate-Binary-Search-Tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            if root.left.val > root.val or root.right.val < root.val:
+                return False
+            else:
+                if self.isValidBST(root.left) and self.isValidBST(root.right):
+                    return True
