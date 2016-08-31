e6d8a04605ec2d6cb0c586d09f80007c90567aea
Haohan Xie <haohanx@umich.edu>
2016-08-31 00:19:03 -0700

    [96. Unique Binary Search Trees][Accepted]committed by LeetCode Extension

---
+++ 96-Unique-Binary-Search-Trees/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+    
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in xrange(1,n+1):
+            for j in xrange(i):
+                res[i] += res[j] * res[i-j-1]
+        return res[n]
