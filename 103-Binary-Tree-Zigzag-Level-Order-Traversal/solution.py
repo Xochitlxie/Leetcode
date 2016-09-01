e3502128432c98cb40fb04e6b48eec7169c76c0e
Haohan Xie <haohanx@umich.edu>
2016-08-31 20:36:24 -0700

    [103. Binary Tree Zigzag Level Order Traversal][Accepted]committed by LeetCode Extension

--- 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.py
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.py
@@ -11,15 +11,19 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        if not root: return []
-        res, temp, stack, flag=[], [], [root], 1
+        result= []
+        if not root:
+            return result
+        temp,stack,flag = [],[root],1
         while stack:
             for i in xrange(len(stack)):
-                node=stack.pop(0)
+                node = stack.pop(0)
                 temp+=[node.val]
-                if node.left: stack+=[node.left]
-                if node.right: stack+=[node.right]
-            res+=[temp[::flag]]
-            temp=[]
-            flag*=-1
-        return res
+                if node.left:
+                    stack+=[node.left]
+                if node.right:
+                    stack+=[node.right]
+            result += [temp[::flag]]
+            temp = []
+            flag *= -1
+        return result
