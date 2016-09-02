e69ca62d629279a8c27b7bd37c1eb86e364ea70c
Haohan Xie <haohanx@umich.edu>
2016-09-02 01:57:24 -0700

    [110. Balanced Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 110-Balanced-Binary-Tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        if not root:
+            return True
+
+        return abs(self.getHeight(root.left) - self.getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+        
