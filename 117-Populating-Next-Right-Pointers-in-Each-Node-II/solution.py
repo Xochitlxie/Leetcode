2ed7e939435cd5c7f77390cf97f3da31e9c73a3d
Haohan Xie <haohanx@umich.edu>
2016-09-09 00:53:51 -0700

    [117. Populating Next Right Pointers in Each Node II][Runtime Error]committed by LeetCode Extension

---
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.py
@@ -0,0 +1,25 @@
+# Definition for binary tree with next pointer.
+# class TreeLinkNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        while root != None:
+            p = root
+            while p!= None and p.left != None:
+                p.left.next = p.right
+                if p.next != None:
+                    p.right.next = p.next.left
+                else:
+                    p.right.next = None
+                p = p.next
+            root = root.left
+        
