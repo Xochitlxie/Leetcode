cf696ae18bf4b56cff6887dd288db6adb984af55
Haohan Xie <haohanx@umich.edu>
2016-09-09 01:04:50 -0700

    [270. Closest Binary Search Tree Value][Accepted]committed by LeetCode Extension

---
+++ 270-Closest-Binary-Search-Tree-Value/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        a = root.val
+        if target < a:
+            kid = root.left
+        else:
+            kid = root.right
+        if not kid: 
+            return a
+        b = self.closestValue(kid,target)
+        return (a,b)[abs(target-a)>abs(target-b)]
+        
