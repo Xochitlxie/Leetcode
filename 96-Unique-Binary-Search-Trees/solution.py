655b7358c2a727eb0af07b3ad5e8fea3e50073ca
Haohan Xie <haohanx@umich.edu>
2016-09-09 13:35:11 -0700

    [96. Unique Binary Search Trees][Accepted]committed by LeetCode Extension

--- 96-Unique-Binary-Search-Trees/solution.py
+++ 96-Unique-Binary-Search-Trees/solution.py
@@ -8,5 +8,5 @@ class Solution(object):
         g[0] = g[1] = 1
         for i in range(2,n+1):
             for j in range(1,i+1):
-                g[i] = g[j-1] * g[i-j]
-        return sum(g[n])
+                g[i] += g[j-1] * g[i-j]
+        return g[n]
