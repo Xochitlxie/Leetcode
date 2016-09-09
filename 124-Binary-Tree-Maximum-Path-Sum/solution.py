9406e9426f3e67d107c43e49af4ff0e50b1e0b7b
Haohan Xie <haohanx@umich.edu>
2016-09-09 00:54:26 -0700

    [124. Binary Tree Maximum Path Sum][Runtime Error]committed by LeetCode Extension

---
+++ 124-Binary-Tree-Maximum-Path-Sum/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        largest = [-2**31]
+        self.helper(root,largest)
+        return self.largest
+        
+    def helper(self, root, largest):
+        if root == None:
+            return 0
+        left = self.helper(root.left, largest)
+        right = self.helper(root.right, largest)
+        largest[0] = max(
+            largest[0], 
+            left + right + root.val, 
+            left + root.val, 
+            right + root.val, 
+            root.val
+        )
+        return max(max(left, right) + root.val, root.val)
+        
