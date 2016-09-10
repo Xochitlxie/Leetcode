26cf4fb1838c32d8a22e9f98115e644a4ce17d46
Haohan Xie <haohanx@umich.edu>
2016-09-10 02:06:36 -0700

    [55. Jump Game][Runtime Error]committed by LeetCode Extension

---
+++ 55-Jump-Game/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxJump = []
+        for i in range(len(nums)):
+            maxJump.append(i+nums[i])
+        i = len(maxJump)-2
+        lastStep = len(maxJump)-1
+        while i > 0:
+            if maxJump[i] >= lastStamp:
+                lastStamp = i
+            i -= 1
+        return lastStamp == 0
+                
+            
