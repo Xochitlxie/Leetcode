93bbb6267ec2f186e4ec63848eaaba703e48bd09
Haohan Xie <haohanx@umich.edu>
2016-09-10 02:49:47 -0700

    [45. Jump Game II][Runtime Error]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        last,current = 0,0
+        njumps,i = 0,0
+        while current < len(nums)-1:
+            while i<= last:
+                current = max(i+nums[i],current)
+                i += 1
+            if last = current:
+                return -1
+            last = current
+            njump+=1
+        return njump
+        
