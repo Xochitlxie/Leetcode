9df3d909183525b3b951bb725a033ba714f9800f
Haohan Xie <haohanx@umich.edu>
2016-09-10 01:11:04 -0700

    [134. Gas Station][Runtime Error]committed by LeetCode Extension

---
+++ 134-Gas-Station/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        start = len(gas) - 1
+        end = 0
+        sum = gas[start] - cost[start]
+        while start > end:
+            if sum >= 0:
+                sum += gas[end] - cost[end]
+                end += 1
+            else:
+                start -= 1
+                sum += gas[start] - cost[start]
+        return sum>=0?start:-1
