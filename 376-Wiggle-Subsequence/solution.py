cc3ac2b79e8eab18be9882134760200db48ca1d8
Haohan Xie <haohanx@umich.edu>
2016-09-10 20:47:01 -0700

    [376. Wiggle Subsequence][Runtime Error]committed by LeetCode Extension

--- 376-Wiggle-Subsequence/solution.py
+++ 376-Wiggle-Subsequence/solution.py
@@ -8,15 +8,19 @@ class Solution(object):
         if len(nums) <= 1:
             return len(nums)
         for i in range(1,len(nums)):
-            diff[i-1] = nums[i] - nums[i-1]
+            diff.append(nums[i] - nums[i-1])
+        if len(nums) <= 1:
+            if nums[0] < 0:
+                return 3
+        prod = []
+        for j in range(1,len(diff)):
+            prud.append(prod[i]*prod[i-1])
         maxLen = 0
         count = 0
-        for num in diff:
-            if num > 0:
+        for num in puod:
+            if num < 0:
                 count += 1
                 maxLen = max(count,maxLen)
             else:
                 count = 0
-        return maxLen
-        
-        
+        return maxLen+2
