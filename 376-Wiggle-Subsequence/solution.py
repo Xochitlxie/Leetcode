b06b3e0e433ccbecdbcab6301a155627da8456ed
Haohan Xie <haohanx@umich.edu>
2016-09-10 23:40:55 -0700

    [376. Wiggle Subsequence][Accepted]committed by LeetCode Extension

--- 376-Wiggle-Subsequence/solution.py
+++ 376-Wiggle-Subsequence/solution.py
@@ -4,23 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        diff = []
-        if len(nums) <= 1:
-            return len(nums)
-        for i in range(1,len(nums)):
-            diff.append(nums[i] - nums[i-1])
-        if len(nums) <= 1:
-            if nums[0] < 0:
-                return 3
-        prod = []
-        for j in range(1,len(diff)):
-            prod.append(diff[i]*diff[i-1])
-        maxLen = 0
-        count = 0
-        for num in prod:
-            if num < 0:
-                count += 1
-                maxLen = max(count,maxLen)
-            else:
-                count = 0
-        return maxLen+2
+        size,f,d = len(nums),1,1
+        for i in range(1,size):
+            if nums[i] > nums[i-1]:
+                f = d+1
+            elif nums[i] < nums[i-1]:
+                d = f+1
+        return min(size,max(f,d))
