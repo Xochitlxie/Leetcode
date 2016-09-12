493e074497c016d8b7fc9f29ec99979d632694be
Haohan Xie <haohanx@umich.edu>
2016-09-12 00:56:32 -0700

    [397. Integer Replacement][Runtime Error]committed by LeetCode Extension

---
+++ 397-Integer-Replacement/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def integerReplacement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1: return counter
+    	if not n%2: return self.integerReplacement(n/2, counter+1)
+    	else: return min(self.integerReplacement(n+1, counter+1), self.integerReplacement(n-1, counter+1))
