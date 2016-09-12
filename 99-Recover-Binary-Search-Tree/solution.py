eb53df8dc1a29c09b76beacb220b816e74235a8a
Haohan Xie <haohanx@umich.edu>
2016-09-11 23:53:34 -0700

    [99. Recover Binary Search Tree][Accepted]committed by LeetCode Extension

--- 99-Recover-Binary-Search-Tree/solution.py
+++ 99-Recover-Binary-Search-Tree/solution.py
@@ -18,18 +18,14 @@ class Solution(object):
             if not root:
                 return
             findNode(root.left)
-            print root.val
             if not self.twoTreeNode[0] and root.val <= self.last.val:
                 self.twoTreeNode[0] = self.last
             if self.twoTreeNode[0] and root.val <= self.last.val:
                 self.twoTreeNode[1] = root
-            print self.twoTreeNode
             self.last = root
             findNode(root.right)
         
         findNode(root)
-        print self.twoTreeNode[0].val
-        print self.twoTreeNode[1].val
         temp = self.twoTreeNode[1].val
         self.twoTreeNode[1].val = self.twoTreeNode[0].val
         self.twoTreeNode[0].val = temp
