5a0cd631dbe0b719dea978338bbea59a51c8cc10
Haohan Xie <haohanx@umich.edu>
2016-09-12 23:07:23 -0700

    [204. Count Primes][Runtime Error]committed by LeetCode Extension

--- 204-Count-Primes/solution.py
+++ 204-Count-Primes/solution.py
@@ -9,6 +9,7 @@ class Solution(object):
         primes = [True] * n
         primes[0] = primes[1] = False
         for i in range(2, int(n**0.5) + 1):
-            for j in range(i*i,n,i):
-                primes[j] = False
+            if prime[i]:
+                for j in range(i*i,n,i):
+                    primes[j] = False
         return sum(primes)
