be7f41466d18d380387ce551802f8a70ac0751ea
Haohan Xie <haohanx@umich.edu>
2016-09-12 18:24:39 -0700

    [212. Word Search II][Accepted]committed by LeetCode Extension

--- 212-Word-Search-II/solution.py
+++ 212-Word-Search-II/solution.py
@@ -1,47 +1,30 @@
-class TrieNode(object):
-    def __init__(self):
-        self.word = None
-        self.children = {}
-
-class Trie(object):
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
-        root = self.root
-        for char in word:
-            root = root.children.setdefault(char, TrieNode())
-        root.word = word
-
 class Solution(object):
-    def search(self, i, j, root, board, m, n, r):
-        char = board[i][j]
-        if not (char and char in root.children):
-            return
-
-        board[i][j], root = None, root.children[char]
-
-        if root.word:
-            r.append(root.word)
-            root.word = None
-
-        for x, y in ((0, -1), (-1, 0), (0, 1), (1, 0)):
-            ii, jj = i + x, j + y
-            if 0 <= ii < m and 0 <= jj < n:
-                self.search(ii, jj, root, board, m, n, r)
-
-        board[i][j] = char
-
     def findWords(self, board, words):
-        if not board:
-            return []
-
-        tree = Trie()
-        [tree.insert(word) for word in words]
-
-        m, n, r = len(board), len(board[0]), []
-
-        for i, row in enumerate(board):
-            for j, char in enumerate(row):
-                self.search(i, j, tree.root, board, m, n, r)
-        return r
+    #make trie
+        trie={}
+        for w in words:
+            t=trie
+            for c in w:
+                if c not in t:
+                    t[c]={}
+                t=t[c]
+            t['#']='#'
+        self.res=set()
+        self.used=[[False]*len(board[0]) for _ in range(len(board))]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                self.find(board,i,j,trie,'')
+        return list(self.res)
+    
+    def find(self,board,i,j,trie,pre):
+        if '#' in trie:
+            self.res.add(pre)
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]):
+            return
+        if not self.used[i][j] and board[i][j] in trie:
+            self.used[i][j]=True
+            self.find(board,i+1,j,trie[board[i][j]],pre+board[i][j])
+            self.find(board,i,j+1,trie[board[i][j]],pre+board[i][j])
+            self.find(board,i-1,j,trie[board[i][j]],pre+board[i][j])
+            self.find(board,i,j-1,trie[board[i][j]],pre+board[i][j])
+            self.used[i][j]=False
