5c962dd27ca3e10b0d134ea467a94fc62c8e31be
Haohan Xie <haohanx@umich.edu>
2016-09-14 14:25:51 -0700

    [4. Median of Two Sorted Arrays][Accepted]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.py
+++ 4-Median-of-Two-Sorted-Arrays/solution.py
@@ -21,21 +21,15 @@ class Solution(object):
             elif i > 0 and j < n and nums1[i-1] > nums2[j]:
                 imax = i - 1
             else:
-                if i == 0:
-                    max_left = nums2[j-1]
-                elif j == 0:
-                    max_left = nums1[i-1]
-                else:
-                    max_left = max(nums1[i-1],nums2[j-1])
+                if i == 0:max_left = nums2[j-1]
+                elif j == 0:max_left = nums1[i-1]
+                else:max_left = max(nums1[i-1],nums2[j-1])
                 
                 if (m+n)%2 == 1:
                     return max_left
-            
-                if i == m:
-                    min_right = nums2[j]
-                elif j == n:
-                    min_right = nums1[i]
-                else:
-                    min_right = min(nums1[i],nums2[j])
+
+                if i == m:min_right = nums2[j]
+                elif j == n:min_right = nums1[i]
+                else:min_right = min(nums1[i],nums2[j])
             
                 return float(max_left+min_right)/2
