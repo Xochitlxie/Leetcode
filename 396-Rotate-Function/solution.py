87d952f1a2887c52049f7860e905d32c4f9e71fa
Haohan Xie <haohanx@umich.edu>
2016-09-15 15:10:27 -0700

    [396. Rotate Function][Accepted]committed by LeetCode Extension

--- 396-Rotate-Function/solution.py
+++ 396-Rotate-Function/solution.py
@@ -7,11 +7,13 @@ class Solution(object):
         if len(A) <= 1:
             return 0
         dp = 0
-        result = -sys.maxint
         total = sum(A)
+        print total
         for i in range(len(A)):
             dp += i * A[i]
+        result = dp
         for j in range(1,len(A)):
-            dp = dp-total+len(A)*A[j-1]
-            result = math.max(dp,result)
-        return max
+            next = dp-total+len(A)*A[j-1]
+            result = max(next,result)
+            dp = next
+        return result
