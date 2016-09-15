7a0b3375d93c9beffd8149127b358b3c11fbe1b5
Haohan Xie <haohanx@umich.edu>
2016-09-15 15:05:32 -0700

    [396. Rotate Function][Runtime Error]committed by LeetCode Extension

--- 396-Rotate-Function/solution.py
+++ 396-Rotate-Function/solution.py
@@ -7,11 +7,11 @@ class Solution(object):
         if len(A) <= 1:
             return 0
         dp = 0
-        max = -sys.maxint
+        result = -sys.maxint
         total = sum(A)
         for i in range(len(A)):
             dp += i * A[i]
         for j in range(1,len(A)):
             dp = dp-total+len(A)*A[j-1]
-            max = max(dp,max)
+            result = max(dp,result)
         return max
