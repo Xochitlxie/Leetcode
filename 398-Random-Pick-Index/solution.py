1d691be769fdc5d9b6e5231ecd9e8f9410d0711b
Haohan Xie <haohanx@umich.edu>
2016-09-19 17:52:51 -0700

    [398. Random Pick Index][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 398-Random-Pick-Index/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        
+        :type nums: List[int]
+        :type numsSize: int
+        """
+        self.l = nums
+
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        index,count,nums = -1,0,self.l
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if index == -1:
+                    index = i
+                    count += 1
+                    continue
+                if random.randint(0,count) == 0:
+                    index = i
+            count += 1
+        return index
+                
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
