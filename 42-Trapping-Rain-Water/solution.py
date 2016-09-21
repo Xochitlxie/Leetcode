5bd094fbd551d101f2991f2697f2f92c1c9a2033
Haohan Xie <haohanx@umich.edu>
2016-09-20 22:19:39 -0700

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.py
+++ 42-Trapping-Rain-Water/solution.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        i,j,max,leftmax,rightmax = 0,len(height)-1,0,0,0
+        i,j,water,leftmax,rightmax = 0,len(height)-1,0,0,0
         while i<= j:
             leftmax = max(leftmax,height[i])
             rightmax= max(rightmax,height[j])
             if height[i] < height[j]:
-                max += (leftmax-height[i])
+                water += (leftmax-height[i])
                 i += 1
             else:
-                max += (rightmax - height[j])
+                water += (rightmax - height[j])
                 j -= 1
-        return max
+        return water
