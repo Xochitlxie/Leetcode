b7b82a34d8831739626a2cf3350f66c1440ebfc9
Haohan Xie <haohanx@umich.edu>
2016-09-21 15:08:32 -0700

    [221. Maximal Square][Runtime Error]committed by LeetCode Extension

--- 221-Maximal-Square/solution.py
+++ 221-Maximal-Square/solution.py
@@ -4,27 +4,16 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int 
         """
-        rows, max_size = len(matrix), 0
-        '''
-        size[i]: the current number of continuous '1's in a column of matrix. Reset when discontinued.
-        The idea is to do a by-row scan, updating size[i]
-        Then check if there are continuous elements in size whose value is bigger than current maximal size.
-        '''
-        if rows > 0:
-            cols = len(matrix[0])
-            size = [0] * cols
-            for x in xrange(rows):
-                # update size
-                count, size = 0, map(lambda x, y: x+1 if y == '1' else 0, size, matrix[x])
-                for y in xrange(cols):
-                    # check if it exceeds current maximal size
-                    if size[y] > max_size:
-                        count += 1
-                        if count > max_size:
-                            # increase maximal size by 1
-                            max_size += 1
-                            break
+        maxSize = 0
+        dp = [[0 for i in range(len(matrix[0])+1)] for j in range(len(matrix)+1)]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == "0":
+                    dp[i+1][j+1] = 0
+                else:
+                    if dp[i][j] > 0 and dp[i+1][j] > 0 and dp[i][j+1]>0:
+                        dp[i+1][j+1] = dp[i][j] + 1
                     else:
-                        count = 0
-
-        return max_size*max_size
+                        dp[i+1][j+1] = 1
+                maxSize = max(maxSize,dp[i+1][j+1])
+        return maxSize*maxSize
