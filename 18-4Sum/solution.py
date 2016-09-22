eac24b09eee4ae7a8b14c53b81416317e6dd71d1
Haohan Xie <haohanx@umich.edu>
2016-09-22 01:16:40 -0700

    [18. 4Sum][Runtime Error]committed by LeetCode Extension

--- 18-4Sum/solution.py
+++ 18-4Sum/solution.py
@@ -1,23 +1,38 @@
 class Solution(object):
-    def fourSum(self, nums, target):
+    def fourSum(self, target, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        # Use DFS to record results
-        if not candidates or not target: return []
-        self.res = []
-        path = []
-        self.DFS(sorted(candidates), path, target)
-        return self.res
-        
-    def DFS(self, candidates, path, target):
-        if target < 0 : return
-        elif target == 0:
-            self.res.append(path)
+        nums.sort()
+        results = []
+        self.findNsum(nums, target, 4, [], results)
+        return results
+
+    def findNsum(self, nums, target, N, result, results):
+        if len(nums) < N or N < 2: return
+
+        # solve 2-sum
+        if N == 2:
+            l,r = 0,len(nums)-1
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    results.append(result + [nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while r > l and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
         else:
-            for i,x in enumerate(candidates):
-                if x <= target: # smart search
-                    self.DFS(candidates[i:], path +[x], target-x) # this guarantee path node is appended in increasing order
-            return
+            for i in range(0, len(nums)-N+1):   # careful about range
+                if target < nums[i]*N or target > nums[-1]*N:  # take advantages of sorted list
+                    break
+                if i == 0 or i > 0 and nums[i-1] != nums[i]:  # recursively reduce N
+                    self.findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+        return
