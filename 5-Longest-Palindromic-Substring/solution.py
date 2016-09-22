af69e6df4c1172bd365cec4c470f349f5fba6bea
Haohan Xie <haohanx@umich.edu>
2016-09-21 20:45:48 -0700

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

---
+++ 5-Longest-Palindromic-Substring/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longest_index = 0
+        max_length = 0
+        for i in xrange(len(s)):
+            if is_palindrome(s, i - max_length, i):
+                longest_index = i - max_length
+                max_length = max_length + 1
+            elif i - max_length - 1 >= 0 and is_palindrome(s, i - max_length - 1, i):
+                longest_index = i - max_length - 1
+                max_length = max_length + 2
+
+        return s[longest_index:longest_index + max_length]
+
+def is_palindrome(string, start, end):
+    for i in xrange((end - start + 1) / 2):
+        if string[start + i] != string[end - i]:
+            return False
+    return True 
