ed4f5474f14576b8afb7f3b3a9fa2664784ae8a7
Haohan Xie <haohanx@umich.edu>
2016-09-23 21:53:45 -0700

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

---
+++ 76-Minimum-Window-Substring/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        m = len(s)
+        n = len(t)
+        if m < n:
+            return ''
+        lt = {}
+        for i in t:
+            if i not in lt:
+                lt[i] = 1
+            else:
+                lt[i] += 1
+        missing = n
+        i = I = J = 0
+        for j, c in enumerate(s, 1):    
+            if c in lt and lt[c] > 0:
+                missing -= 1
+            if c in lt:
+                lt[c] -= 1
+
+            while i < j and not missing:
+                if not J or j-i < J-I:
+                    I, J = i, j
+                if s[i] not in lt:
+                    i += 1
+                    continue
+                else:
+                    lt[s[i]] += 1
+                    if lt[s[i]] > 0:
+                        missing += 1
+                    i += 1
+        return s[I:J]
