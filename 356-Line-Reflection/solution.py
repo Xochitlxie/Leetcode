befda8455bfdc50ddf1da6cab609acc797563005
Haohan Xie <haohanx@umich.edu>
2016-09-25 11:33:04 -0700

    [356. Line Reflection][Runtime Error]committed by LeetCode Extension

---
+++ 356-Line-Reflection/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isReflected(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        mid = (min(points)[0] + max(points)[0])*1.0/2
+        count = {}
+        for point in points:
+            if point[0] == mid:
+                continue
+            elif point[0] < mid:
+                count[(mid-point[0],point[1])] = count.get((mid-point[0],point[1]),0) + 1
+            else:
+                if (point[0] - mid,point[1]) not in count:
+                    return False
+                else:
+                    count[(point[0] - mid,point[1])] -= 1
+        for key in count:
+            if count[key] != 0:
+                return False
+        return True
