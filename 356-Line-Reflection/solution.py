85fbfebe0c426f6d86285557accb665da3cce0c1
Haohan Xie <haohanx@umich.edu>
2016-09-27 22:39:57 -0700

    [356. Line Reflection][Accepted]committed by LeetCode Extension

--- 356-Line-Reflection/solution.py
+++ 356-Line-Reflection/solution.py
@@ -6,16 +6,19 @@ class Solution(object):
         """
         if len(points) <= 1: return True
         mid = (min(points)[0] + max(points)[0])*1.0/2
-        count = {}
-        k = list(set(points))
-        for point in k:
+        leftBucket = set()
+        rightBucket = set()
+        for point in points:
             if point[0] == mid:
                 continue
             elif point[0] < mid:
-                count[(mid-point[0],point[1])] = count.get((mid-point[0],point[1]),0) + 1
+                leftBucket.add((mid-point[0],point[1]))
             else:
-                count[(point[0] - mid,point[1])] = count.get((point[0] - mid,point[1]),0) -1
-        for key in count:
-            if count[key] != 0:
+                rightBucket.add((point[0] - mid,point[1]))
+        if len(leftBucket)!= len(rightBucket):
+            return False
+        while leftBucket:
+            ele = leftBucket.pop()
+            if ele not in rightBucket:
                 return False
         return True
