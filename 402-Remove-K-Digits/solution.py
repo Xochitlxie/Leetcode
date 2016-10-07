409d381a761c11c52bb903fba686d4e4cb73595b
Haohan Xie <haohanx@umich.edu>
2016-10-06 17:51:47 -0700

    [402. Remove K Digits][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 402-Remove-K-Digits/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        if k == len(num):
+            return "0"
+        stack = []
+        for i in num:
+            if k>0 and stack and stack[-1] > i:
+                stack.pop()
+                k -= 1
+            stack.append(i)
+        while stack:
+            i = 0
+            if stack[i] == "0":
+                i += 1
+            else:
+                break
+        return ('').join(stack[i:])
