7088ad55d295e06d4585a7a3e2a1a5fd71ae0574
Haohan Xie <haohanx@umich.edu>
2016-10-07 18:19:18 -0700

    [135. Candy][Accepted]committed by LeetCode Extension

--- 135-Candy/solution.py
+++ 135-Candy/solution.py
@@ -8,10 +8,8 @@ class Solution(object):
         for i in range(len(ratings)-1):
             if ratings[i] < ratings[i+1]:
                 res[i+1] = res[i] + 1
-        print res
         for j in range(-1,-len(ratings),-1):
             if ratings[j] < ratings[j-1]:
                 if res[j-1] < res[j] + 1:
                     res[j-1] = res[j] + 1
-        print res
         return sum(res)
