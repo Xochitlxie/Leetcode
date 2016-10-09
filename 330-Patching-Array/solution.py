91600336b8868c30322cbb8e4c1e1d25bcae5b62
Haohan Xie <haohanx@umich.edu>
2016-10-09 00:28:15 -0700

    [330. Patching Array][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 330-Patching-Array/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        miss,added,i = 0,0,0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                i += 1
+                miss += nums[i]
+            
+            else:
+                miss += miss
+                added += 1
+        return added
