f4748c4e93efd1c8e0cdfa6550ae163f1bc818fe
Haohan Xie <haohanx@umich.edu>
2016-10-10 00:08:01 -0700

    [321. Create Maximum Number][Accepted]committed by LeetCode Extension

--- 321-Create-Maximum-Number/solution.py
+++ 321-Create-Maximum-Number/solution.py
@@ -6,37 +6,18 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        n, m= len(nums1),len(nums2)
-        ret = [0] * k
-        for i in range(0, k+1):
-            j = k - i
-            if i > n or j > m: continue
-            left = self.maxSingleNumber(nums1, i)
-            right = self.maxSingleNumber(nums2, j)
-            num = self.mergeMax(left, right)
-            ret = max(num, ret)
-        return ret
 
+        def prep(nums, k):
+            drop = len(nums) - k
+            out = []
+            for num in nums:
+                while drop and out and out[-1] < num:
+                    out.pop()
+                    drop -= 1
+                out.append(num)
+            return out[:k]
 
-    def mergeMax(self, nums1, nums2):
-        ans = []
-        while nums1 or nums2:
-            if nums1 > nums2:
-                ans += nums1[0],
-                nums1 = nums1[1:]
-            else:
-                ans += nums2[0],
-                nums2 = nums2[1:]
-        return ans
+        def merge(a, b):
+            return [max(a, b).pop(0) for _ in a+b]
 
-    def maxSingleNumber(self, nums, selects):
-        n = len(nums)
-        ret = [-1]
-        if selects > n : return ret
-        while selects > 0:
-            start = ret[-1] + 1 #search start
-            end = n-selects + 1 #search end
-            ret.append( max(range(start, end), key = nums.__getitem__))
-            selects -= 1
-        ret = [nums[item] for item in ret[1:]]
-        return ret
+        return max(merge(prep(nums1, i), prep(nums2, k-i)) for i in range(k+1) if i <= len(nums1) and k-i <= len(nums2))
